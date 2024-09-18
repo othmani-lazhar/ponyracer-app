0dd4c99d146b41b23814aa65b6b5bdf445f63e7c
othma <othmani.lazhar@gmail.com>
2024-09-18 08:16:45 +0200

    add menu component

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pr-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+  navbarCollapsed = true;
+
+  toggleNavbar() {
+    this.navbarCollapsed = !this.navbarCollapsed;
+  }
+}
